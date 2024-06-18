553402d5d53eba7dc705c4346eb74218cf496bab
rafiqulislamrobin <kratosrobin467@gmail.com>
2024-06-18 12:45:54 +0600

    restructured folder and renamed also add migration for seed in npsql removed sqlite

--- Carties-Auction/IdentityService/SeedData.cs
+++ Carties-Auction/IdentityService/SeedData.cs
@@ -18,8 +18,11 @@ namespace IdentityService
                 context.Database.Migrate();
 
                 var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                if (userMgr.Users.Any()) return;
+
                 var alice = userMgr.FindByNameAsync("alice").Result;
-                if (false)
+                if (alice == null)
                 {
                     alice = new ApplicationUser
                     {
@@ -35,9 +38,6 @@ namespace IdentityService
 
                     result = userMgr.AddClaimsAsync(alice, new Claim[]{
                                 new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                                new Claim(JwtClaimTypes.GivenName, "Alice"),
-                                new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                                new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
                             }).Result;
                     if (!result.Succeeded)
                     {
@@ -51,7 +51,7 @@ namespace IdentityService
                 }
 
                 var bob = userMgr.FindByNameAsync("bob").Result;
-                if (false)
+                if (bob == null)
                 {
                     bob = new ApplicationUser
                     {
@@ -67,9 +67,6 @@ namespace IdentityService
 
                     result = userMgr.AddClaimsAsync(bob, new Claim[]{
                                 new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                                new Claim(JwtClaimTypes.GivenName, "Bob"),
-                                new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                                new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
                                 new Claim("location", "somewhere")
                             }).Result;
                     if (!result.Succeeded)
