4cb326807c0ca57bdbcb17650617cfed3be2c986
rafiqulislamrobin <kratosrobin467@gmail.com>
2024-06-29 16:20:29 +0600

    fontend added

---
+++ Frontend/nextjs-cartiest/app/nav/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { AiOutlineCar } from "react-icons/ai";
+
+export default function navbar() {
+  return (
+    <header
+      className="
+    sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
+  "
+    >
+      <div className="flex items-center gap-2 text-3xl font-semibold text-purple-500">
+        <AiOutlineCar />
+        <div >
+          Carties Auction
+        </div>
+      </div>
+      <div>Search</div>
+      <div>login</div>
+    </header>
+  );
+}
---
+++ Frontend/nextjs-cartiest/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Listings from "./auction/Listings";
+
+export default function Home() {
+  return (
+    <h3 >
+      <Listings/> 
+
+    </h3>
+  );
+}
