63d7adf79ab0f3496afd09881034c021022c892f
rafiqulislamrobin <kratosrobin467@gmail.com>
2024-06-29 17:29:06 +0600

    fronted auction card desgin updated

--- Frontend/nextjs-cartiest/app/auction/Listings.tsx
+++ Frontend/nextjs-cartiest/app/auction/Listings.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import AuctionCard from './AuctionCard';
 
-async function getData(){
-    const res = await fetch('http://localhost:6001/search');
-    console.log(res);
-    if(!res.ok) throw new Error('Failed to fetch data');
+async function getData() {
+  const res = await fetch('http://localhost:6001/search?pageSize=20');
+  if (!res.ok) throw new Error('Failed to fetch data');
 
-    return res.json();
+  return res.json();
 }
 
+
 export default async function Listings() {
+
+  
     const data = await getData();
-  return (
-    <div>{JSON.stringify(data, null, 2)}</div>
-  )
+    return (
+      <div className='grid grid-cols-4 gap-6'>
+        {data && data.results.map((auction: any) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+      </div>
+    );
 }
